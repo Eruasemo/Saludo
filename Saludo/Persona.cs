32384e15b42e13a088695f9b770fc95afe9dc8aa
Manuel Balderas <tassadar46@gmail.com>
2024-08-19 12:54:30 -0600

    Agregar archivos de proyecto.

---
+++ Saludo/Persona.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saludo
+{
+    internal class Persona
+    {
+        private string name;
+        private int age;
+
+        public Persona(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Persona()
+        {
+            this.name = "Fulanito";
+            this.age = 18;
+        }
+
+        public string Saludar()
+        {
+            string saludo = "Hola soy " + this.name + " y tengo " + this.age + " años de edad.";
+            return saludo;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+
+            set { this.name = value; }
+        }
+
+        public override string ToString()
+        {
+            string saludo = "Hola soy " + this.name + " y tengo " + this.age + " años de edad.";
+            return saludo;
+        }
+    }
+}
---
+++ Saludo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saludo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Persona persona = new Persona("Manuel", 38);
+            Console.WriteLine(persona.Name);
+            persona.Name = "Raul";
+            Console.WriteLine(persona);
+
+
+
+            Persona persona2 = new Persona();
+
+            persona2.Saludar();
+        }
+    }
+}
